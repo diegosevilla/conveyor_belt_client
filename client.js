5d96c187443e42b7c4b859e42f5744c2df0cd242
Jose Diego  O. Sevilla <josevilla1@up.edu.ph>
2018-02-12 21:19:37 +0800

    Update socket.io connection

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-var socket = require('socket.io-client')('http://localhost:3000');
+var socket = require('socket.io-client')('https://conveyor-belt-controller.herokuapp.com');
 var fs = require('fs');
 
 var controller = {};
